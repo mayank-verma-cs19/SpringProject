651770c16f82e24166026d8f1382f224ee7f7fbf
Mayank Verma <mkverma22474@gmail.com>
2024-09-05 01:50:54 +0530

    Initial Commit

---
+++ demo/src/main/java/com/example/demo/controller/StudentRestController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.StudentDto;
+import com.example.demo.service.StudentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin("*")
+@RestController()
+@RequestMapping("api/student")
+@AllArgsConstructor
+public class StudentRestController {
+
+    private StudentService studentService;
+
+    @PostMapping
+    public ResponseEntity<StudentDto> createStudent(@RequestBody StudentDto studentDto) {
+        StudentDto savedStudent = studentService.createStudent(studentDto);
+        return new ResponseEntity<>(savedStudent, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<StudentDto>> getAllStudents() {
+        List<StudentDto> getStudent = studentService.getAllStudents();
+        return new ResponseEntity<>(getStudent, HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<StudentDto> getStudentById(@PathVariable Long id) {
+        StudentDto studentDto = studentService.getStudentById(id);
+        return new ResponseEntity<>(studentDto, HttpStatus.OK);
+    }
+}
