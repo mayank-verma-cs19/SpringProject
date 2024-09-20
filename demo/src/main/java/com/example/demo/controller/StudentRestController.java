1a7363a68bf787551e06c6f0786fe2faf130ee5c
Mayank Verma <mkverma22474@gmail.com>
2024-09-20 10:29:22 +0530

    Added stream question

--- demo/src/main/java/com/example/demo/controller/StudentRestController.java
+++ demo/src/main/java/com/example/demo/controller/StudentRestController.java
@@ -18,19 +18,19 @@ public class StudentRestController {
 
     private StudentService studentService;
 
-    @PostMapping
+    @PostMapping("/saveStudent")
     public ResponseEntity<StudentDto> createStudent(@RequestBody StudentDto studentDto) {
         StudentDto savedStudent = studentService.createStudent(studentDto);
         return new ResponseEntity<>(savedStudent, HttpStatus.CREATED);
     }
 
-    @GetMapping
+    @GetMapping("getAllStudents")
     public ResponseEntity<List<StudentDto>> getAllStudents() {
         List<StudentDto> getStudent = studentService.getAllStudents();
         return new ResponseEntity<>(getStudent, HttpStatus.OK);
     }
 
-    @GetMapping("{id}")
+    @GetMapping("getById")
     public ResponseEntity<StudentDto> getStudentById(@PathVariable Long id) {
         StudentDto studentDto = studentService.getStudentById(id);
         return new ResponseEntity<>(studentDto, HttpStatus.OK);
